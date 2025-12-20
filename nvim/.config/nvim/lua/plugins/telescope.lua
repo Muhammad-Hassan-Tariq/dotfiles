2c0f7cda64b300bc5a3ffd16f766c12380616fa4
Muhammad Hassan Tariq <hassantariqjms@gmail.com>
2025-12-20 16:47:44 +0500

    Update rice 🍚

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,15 +1,24 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  opts = {
-    defaults = {
-      -- This makes sure the preview window is big enough to see your code!
-      layout_strategy = "horizontal",
-      layout_config = { preview_width = 0.6 },
-    },
-    pickers = {
-      colorscheme = {
-        enable_preview = true, -- 🎯 This is the magic line for Hassan!
-      },
-    },
-  },
+    "nvim-telescope/telescope.nvim",
+    opts = function(_, opts)
+        -- 1. Merge the base options
+        opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
+            layout_strategy = "horizontal",
+            layout_config = { preview_width = 0.6 },
+        })
+
+        -- 2. Force the find_files picker to be AGGRESSIVE 🥋
+        opts.pickers = opts.pickers or {}
+        opts.pickers.find_files = {
+            hidden = true,
+            no_ignore = true,
+            -- This ensures it searches from your CURRENT directory, not the "Project Root"
+            cwd = vim.loop.cwd(),
+        }
+
+        -- 3. Keep your magic colorscheme preview 🎯
+        opts.pickers.colorscheme = {
+            enable_preview = true,
+        }
+    end,
 }
