59d3eb97394c0306b00c81436f32abbf4fc8a74a
Muhammad Hassan Tariq <hassantariqjms@gmail.com>
2026-01-13 19:03:04 +0500

    Update rice

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,15 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Append semicolon at end of line and start a new line below
-vim.keymap.set("i", ";;", "<Esc>A;<CR>", {
-    desc = "Append semicolon and move to new line",
-    noremap = true,
-    silent = true,
-})
-
 -- Jump to next line while adding semicolon
 vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit Insert Mode" })
-
--- Scrool down and center the screen
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center view" })
