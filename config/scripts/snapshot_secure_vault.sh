7f9dc2613c811e9dced3399fad4c9da966f3e715
eagle <feraleagle@protonmail.com>
2026-05-26 14:06:38 +0500

    Updated

---
+++ config/scripts/snapshot_secure_vault.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+SRC="$HOME/.secure_vault"
+SNAPS="$HOME/.secure_vault/.snapshots"
+
+mkdir -p "$SNAPS/n-1" "$SNAPS/n-2" "$SNAPS/n-3"
+
+# shift snapshots safely
+rm -rf "$SNAPS/n-3/"*
+mv "$SNAPS/n-2/"* "$SNAPS/n-3/" 2>/dev/null
+mv "$SNAPS/n-1/"* "$SNAPS/n-2/" 2>/dev/null
+
+# create new snapshot
+rsync -av --exclude ".*" "$SRC/" "$SNAPS/n-1/"
