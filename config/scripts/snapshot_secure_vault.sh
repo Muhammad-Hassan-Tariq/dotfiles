87a935104f953935d82662c29ee2995144145ddb
eagle <feraleagle@protonmail.com>
2026-05-26 14:12:37 +0500

    Updated

--- config/scripts/snapshot_secure_vault.sh
+++ config/scripts/snapshot_secure_vault.sh
@@ -8,8 +8,7 @@ SNAPS="$HOME/.secure_vault/.snapshots"
 mkdir -p "$SNAPS/n-1" "$SNAPS/n-2" "$SNAPS/n-3"
 
 # shift snapshots safely
-rm -rf "$SNAPS/n-3/"*
-mv "$SNAPS/n-2/"* "$SNAPS/n-3/" 2>/dev/null
+rm -rf "$SNAPS/n-2/"*
 mv "$SNAPS/n-1/"* "$SNAPS/n-2/" 2>/dev/null
 
 # create new snapshot
